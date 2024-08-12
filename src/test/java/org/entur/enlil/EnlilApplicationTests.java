a597246b843cbf4442c20d7b5d8734432d2eb076
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-08-12 11:27:29 +0200

    Run firestore emulator with testcontainer

--- src/test/java/org/entur/enlil/EnlilApplicationTests.java
+++ src/test/java/org/entur/enlil/EnlilApplicationTests.java
@@ -1,10 +1,42 @@
 package org.entur.enlil;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.FirestoreEmulatorContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class EnlilApplicationTests {
+    @Container
+    private static final FirestoreEmulatorContainer firestoreEmulator =
+            new FirestoreEmulatorContainer(
+                    DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk:317.0.0-emulators"));
+
+    @DynamicPropertySource
+    static void emulatorProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.cloud.gcp.firestore.host-port", firestoreEmulator::getEmulatorEndpoint);
+    }
+
+    @TestConfiguration
+    static class EmulatorConfiguration {
+        // By default, autoconfiguration will initialize application default credentials.
+        // For testing purposes, don't use any credentials. Bootstrap w/ NoCredentialsProvider.
+        @Bean
+        CredentialsProvider googleCredentials() {
+            return NoCredentialsProvider.create();
+        }
+    }
 
     @Test
     void contextLoads() {
