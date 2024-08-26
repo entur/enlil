4e0cd1e1ed88d79f090287cbd68200f3e189f6fa
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-08-26 22:59:33 +0200

    Add test for extrajourneys, and missing snapshots

--- src/test/java/org/entur/enlil/EnlilApplicationIntegrationTests.java
+++ src/test/java/org/entur/enlil/EnlilApplicationIntegrationTests.java
@@ -169,7 +169,8 @@ class EnlilApplicationIntegrationTests {
   }
 
   @Test
-  void testCancellation() throws ExecutionException, InterruptedException {
+  void testEstimatedTimetableRequestWithCancellation()
+    throws ExecutionException, InterruptedException {
     firestore
       .collection("codespaces/TST/authorities/TST:Authority:TST/cancellations")
       .add(createTestCancellation())
@@ -231,7 +232,7 @@ class EnlilApplicationIntegrationTests {
     return cancellation;
   }
 
-  private static EstimatedVehicleJourneyEntity.@NotNull EstimatedCalls createTestEstimatedCalls() {
+  private EstimatedVehicleJourneyEntity.@NotNull EstimatedCalls createTestEstimatedCalls() {
     EstimatedVehicleJourneyEntity.EstimatedCalls estimatedCalls =
       new EstimatedVehicleJourneyEntity.EstimatedCalls();
     EstimatedVehicleJourneyEntity.EstimatedCall call1 =
@@ -254,6 +255,90 @@ class EnlilApplicationIntegrationTests {
     call2.setArrivalBoardingActivity("noAlighting");
     call2.setDepartureStatus("cancelled");
     call2.setDepartureBoardingActivity("boarding");
+    estimatedCalls.setEstimatedCall(List.of(call1, call2));
+    return estimatedCalls;
+  }
+
+  @Test
+  void testEstimatedTimetableRequestWithExtraJourney()
+    throws ExecutionException, InterruptedException {
+    firestore
+      .collection("codespaces/TST/authorities/TST:Authority:TST/extrajourneys")
+      .add(createTestExtraJourney())
+      .get();
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setAccept(List.of(MediaType.APPLICATION_XML));
+    headers.setContentType(MediaType.APPLICATION_XML);
+    RequestEntity<String> requestEntity = new RequestEntity<>(
+      """
+                          <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                          <Siri version="2.0" xmlns="http://www.siri.org.uk/siri" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns:ns3="http://www.ifopt.org.uk/ifopt" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0">
+                            <ServiceRequest>
+                              <RequestTimestamp>2019-11-06T14:45:00+01:00</RequestTimestamp>
+                              <RequestorRef>ENTUR_DEV-1</RequestorRef>
+                              <EstimatedTimetableRequest version="2.0">
+                                <RequestTimestamp>2019-11-06T14:45:00+01:00</RequestTimestamp>
+                                <MessageIdentifier>e11d9efb-ee7b-4a67-847a-a254e813f0da</MessageIdentifier>
+                              </EstimatedTimetableRequest>
+                            </ServiceRequest>
+                          </Siri>
+                          """,
+      headers,
+      HttpMethod.POST,
+      URI.create("http://localhost:" + randomPort + "/siri"),
+      Siri.class
+    );
+
+    ResponseEntity<String> response = restTemplate.exchange(requestEntity, String.class);
+
+    expect.toMatchSnapshot(response.getBody());
+  }
+
+  private EstimatedVehicleJourneyEntity createTestExtraJourney() {
+    var extraJourney = new EstimatedVehicleJourneyEntity();
+    var estimatedVehicleJourney =
+      new EstimatedVehicleJourneyEntity.EstimatedVehicleJourney();
+    estimatedVehicleJourney.setRecordedAtTime(
+      ISO_DATE_TIME.format(OffsetDateTime.now(clock))
+    );
+    estimatedVehicleJourney.setLineRef("TST:Line:1");
+    estimatedVehicleJourney.setDirectionRef("0");
+    estimatedVehicleJourney.setDataSource("TST");
+    estimatedVehicleJourney.setEstimatedCalls(createExtraJourneyTestEstimatedCalls());
+    estimatedVehicleJourney.setIsCompleteStopSequence(true);
+    estimatedVehicleJourney.setExpiresAtEpochMs(
+      Instant.now(clock).plus(20, ChronoUnit.MINUTES).toEpochMilli()
+    );
+
+    estimatedVehicleJourney.setExternalLineRef("TST:Line:1");
+    estimatedVehicleJourney.setGroupOfLinesRef("TST:Network:1");
+    estimatedVehicleJourney.setOperatorRef("TST:Operator:1");
+    estimatedVehicleJourney.setPublishedLineName("Test");
+    estimatedVehicleJourney.setRouteRef("TST:Route:1");
+    estimatedVehicleJourney.setVehicleMode("bus");
+
+    extraJourney.setEstimatedVehicleJourney(estimatedVehicleJourney);
+    return extraJourney;
+  }
+
+  private EstimatedVehicleJourneyEntity.EstimatedCalls createExtraJourneyTestEstimatedCalls() {
+    EstimatedVehicleJourneyEntity.EstimatedCalls estimatedCalls =
+      new EstimatedVehicleJourneyEntity.EstimatedCalls();
+    EstimatedVehicleJourneyEntity.EstimatedCall call1 =
+      new EstimatedVehicleJourneyEntity.EstimatedCall();
+    call1.setStopPointRef("TST:Quay:1");
+    call1.setOrder(1);
+    call1.setStopPointName("Fjord");
+    call1.setDepartureBoardingActivity("boarding");
+    EstimatedVehicleJourneyEntity.EstimatedCall call2 =
+      new EstimatedVehicleJourneyEntity.EstimatedCall();
+    call2.setStopPointRef("TST:Quay:2");
+    call2.setOrder(2);
+    call2.setStopPointName("Fjell");
+    call2.setArrivalBoardingActivity("alighting");
     estimatedCalls.setEstimatedCall(List.of(call1, call2));
     return estimatedCalls;
   }
