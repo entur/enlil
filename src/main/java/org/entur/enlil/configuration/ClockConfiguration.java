65d7b0ad7855002bfe111698c477a656ef5ec898
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-09-13 21:12:22 +0200

    Formatting

--- src/main/java/org/entur/enlil/configuration/ClockConfiguration.java
+++ src/main/java/org/entur/enlil/configuration/ClockConfiguration.java
@@ -1,16 +1,16 @@
 package org.entur.enlil.configuration;
 
+import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.time.Clock;
-
 @Configuration
 @Profile("!test")
 public class ClockConfiguration {
-    @Bean
-    Clock clock() {
-        return Clock.systemDefaultZone();
-    }
+
+  @Bean
+  Clock clock() {
+    return Clock.systemDefaultZone();
+  }
 }
--- src/main/java/org/entur/enlil/configuration/SchedulingConfiguration.java
+++ src/main/java/org/entur/enlil/configuration/SchedulingConfiguration.java
@@ -7,5 +7,4 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @Profile("scheduling")
 @EnableScheduling
-public class SchedulingConfiguration {
-}
+public class SchedulingConfiguration {}
