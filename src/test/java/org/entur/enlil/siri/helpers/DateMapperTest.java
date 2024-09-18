4c7641b92801366cae8f226e3438bccf167ee64a
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-09-18 14:10:37 +0200

    Formatting

--- src/test/java/org/entur/enlil/siri/helpers/DateMapperTest.java
+++ src/test/java/org/entur/enlil/siri/helpers/DateMapperTest.java
@@ -1,23 +1,23 @@
 package org.entur.enlil.siri.helpers;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class DateMapperTest {
 
-    @Test
-    void testMapZonedDateTimeToString() {
-        Clock clock = Mockito.mock(Clock.class);
-        Mockito.when(clock.instant()).thenReturn(Instant.parse("2023-09-19T19:19:00Z"));
-        Mockito.when(clock.getZone()).thenReturn(ZoneId.of("UTC"));
-        Assertions.assertEquals("2023-09-19T19:19:00Z", DateMapper.mapZonedDateTimeToString(
-                ZonedDateTime.now(clock)
-        ));
-    }
+  @Test
+  void testMapZonedDateTimeToString() {
+    Clock clock = Mockito.mock(Clock.class);
+    Mockito.when(clock.instant()).thenReturn(Instant.parse("2023-09-19T19:19:00Z"));
+    Mockito.when(clock.getZone()).thenReturn(ZoneId.of("UTC"));
+    Assertions.assertEquals(
+      "2023-09-19T19:19:00Z",
+      DateMapper.mapZonedDateTimeToString(ZonedDateTime.now(clock))
+    );
+  }
 }
