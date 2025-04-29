9f489a308415aacb6598ef65845cfaef20cfc402
Vincent Paturet <vincent.paturet@entur.org>
2025-04-29 16:28:55 +0200

    Add support for permission store proxy

--- src/main/java/org/entur/enlil/security/EnturSecurityConfiguration.java
+++ src/main/java/org/entur/enlil/security/EnturSecurityConfiguration.java
@@ -2,18 +2,24 @@ package org.entur.enlil.security;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.entur.enlil.security.spi.UserContextService;
+import org.entur.oauth2.AuthorizedWebClientBuilder;
 import org.entur.oauth2.JwtRoleAssignmentExtractor;
 import org.entur.oauth2.RorAuthenticationConverter;
 import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolverBuilder;
 import org.entur.oauth2.user.JwtUserInfoExtractor;
+import org.entur.ror.permission.RemoteBabaRoleAssignmentExtractor;
 import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.rutebanken.helper.organisation.user.UserInfoExtractor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @Profile("entur")
@@ -24,9 +30,43 @@ public class EnturSecurityConfiguration {
     return new RorAuthenticationConverter();
   }
 
+  @ConditionalOnProperty(
+    value = "enlil.security.role.assignment.extractor",
+    havingValue = "jwt",
+    matchIfMissing = true
+  )
   @Bean
-  public RoleAssignmentExtractor roleAssignmentExtractor() {
+  public RoleAssignmentExtractor jwtRoleAssignmentExtractor() {
     return new JwtRoleAssignmentExtractor();
+  }
+
+  @ConditionalOnProperty(
+    value = "enlil.security.role.assignment.extractor",
+    havingValue = "baba"
+  )
+  @Bean
+  public RoleAssignmentExtractor babaRoleAssignmentExtractor(
+    @Qualifier("internalWebClient") WebClient webClient,
+    @Value("${user.permission.rest.service.url}") String url
+  ) {
+    return new RemoteBabaRoleAssignmentExtractor(webClient, url);
+  }
+
+  @ConditionalOnProperty(
+    value = "enlil.security.role.assignment.extractor",
+    havingValue = "baba"
+  )
+  @Bean("internalWebClient")
+  WebClient internalWebClient(
+    WebClient.Builder webClientBuilder,
+    OAuth2ClientProperties properties,
+    @Value("${ror.oauth2.client.audience}") String audience
+  ) {
+    return new AuthorizedWebClientBuilder(webClientBuilder)
+      .withOAuth2ClientProperties(properties)
+      .withAudience(audience)
+      .withClientRegistrationId("internal")
+      .build();
   }
 
   @Bean
